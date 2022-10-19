0a7e041054b35ca62cf100fb9e70ccccc203b0f6
Nmani <111077779+mani000007@users.noreply.github.com>
2022-10-19 21:08:15 +0530

    Update Excel_xlsx.py

--- Excel_xlsx.py
+++ Excel_xlsx.py
@@ -1,9 +1,11 @@
 import pandas as pd
 location = 'E:\mani\employees.xlsx'
 df = pd.read_excel(location)
-d = df.sort_values('Years_of_experience')
+d = df.sort_values('Years_of_experience', ascending=True)
+d1 = df.sort_values('Years_of_experience', ascending=False)
 print("Before Sorting: \n\n", df, "\n\n")
-print("After Sorting: \n\n", d)
+print("Ascending Order: \n\n", d, "\n\n")
+print("Descending Order: \n\n",d1)
 
 
 ###---Output---###
@@ -19,7 +21,7 @@ Before Sorting:
  ram                   5                 Testing       1997-08-27 
 
 
-After Sorting:
+Ascending Order:
 
 
 Name         Years_of_experience    Job_Title     Date_of_Birth
@@ -28,5 +30,16 @@ Mani                2                 DevOps        1999-08-19
 siva                3                 MuleSoft      1998-12-09
 kumar               4                 Devloper      1999-09-19
 ram                 5                 Testing       1997-08-27
+
+
+Descending Order:
+ 
+  Name         Years_of_experience     Job_Title    Date_of_Birth
+  ram                    5             Testing       1997-08-27
+  kumar                  4             Devloper      1999-09-19
+  siva                   3             MuleSoft      1998-12-09
+  Mani                   2             DevOps        1999-08-19
+  mkr                    1             DevOps        2000-08-12
+
 
 
