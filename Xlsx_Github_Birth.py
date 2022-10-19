805ef872ddba481220cae8e66e737ba17fd546f0
Nmani <111077779+mani000007@users.noreply.github.com>
2022-10-19 10:31:04 +0530

    modified

--- Xlsx_Github_Birth.py
+++ Xlsx_Github_Birth.py
@@ -66,101 +66,6 @@ for i in range(0, len(json)):
    
 ####---OUTPUT---####
 
-
-
-E:\mani>python main.py
-Enter the github username:mani000007
-Project Number: 1
-Project Name: All_in_one
-Project URL: https://github.com/mani000007/All_in_one
-
-Project Number: 2
-Project Name: example-voting-app-kubernetes
-Project URL: https://github.com/mani000007/example-voting-app-kubernetes
-
-Project Number: 3
-Project Name: gith
-Project URL: https://github.com/mani000007/gith
-
-Project Number: 4
-Project Name: git_files
-Project URL: https://github.com/mani000007/git_files
-
-Project Number: 5
-Project Name: ignorefiles
-Project URL: https://github.com/mani000007/ignorefiles
-
-Project Number: 6
-Project Name: mani
-Project URL: https://github.com/mani000007/mani
-
-Project Number: 7
-Project Name: manik
-Project URL: https://github.com/mani000007/manik
-
-Project Number: 8
-Project Name: manimk
-Project URL: https://github.com/mani000007/manimk
-
-Project Number: 9
-Project Name: Mconflict
-Project URL: https://github.com/mani000007/Mconflict
-
-Project Number: 10
-Project Name: mgit
-Project URL: https://github.com/mani000007/mgit
-
-Project Number: 11
-Project Name: mkr
-Project URL: https://github.com/mani000007/mkr
-
-Project Number: 12
-Project Name: myPython
-Project URL: https://github.com/mani000007/myPython
-
-Project Number: 13
-Project Name: play-with-docker
-Project URL: https://github.com/mani000007/play-with-docker
-
-Project Number: 14
-Project Name: pull-request-demo
-Project URL: https://github.com/mani000007/pull-request-demo
-
-Project Number: 15
-Project Name: pyPrograms
-Project URL: https://github.com/mani000007/pyPrograms
-
-Project Number: 16
-Project Name: Rebase
-Project URL: https://github.com/mani000007/Rebase
-
-Project Number: 17
-Project Name: reponew
-Project URL: https://github.com/mani000007/reponew
-
-Project Number: 18
-Project Name: revertgit
-Project URL: https://github.com/mani000007/revertgit
-
-Repository(full_name="mani000007/All_in_one")
-Repository(full_name="mani000007/example-voting-app-kubernetes")
-Repository(full_name="mani000007/gith")
-Repository(full_name="mani000007/git_files")
-Repository(full_name="mani000007/ignorefiles")
-Repository(full_name="mani000007/mani")
-Repository(full_name="mani000007/manik")
-Repository(full_name="mani000007/manimk")
-Repository(full_name="mani000007/Mconflict")
-Repository(full_name="mani000007/mgit")
-Repository(full_name="mani000007/mkr")
-Repository(full_name="mani000007/myPython")
-Repository(full_name="mani000007/play-with-docker")
-Repository(full_name="mani000007/pull-request-demo")
-Repository(full_name="mani000007/pyPrograms")
-Repository(full_name="mani000007/Rebase")
-Repository(full_name="mani000007/reponew")
-Repository(full_name="mani000007/revertgit")
-
 E:\mani>python main.py
 Enter the github username:mani000007
 Repository(full_name="mani000007/All_in_one")
@@ -181,6 +86,8 @@ Repository(full_name="mani000007/pyPrograms")
 Repository(full_name="mani000007/Rebase")
 Repository(full_name="mani000007/reponew")
 Repository(full_name="mani000007/revertgit")
+
+
 Project Number: 1
 Project Name: All_in_one
 Project URL: https://github.com/mani000007/All_in_one
