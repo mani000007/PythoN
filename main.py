f811ea027ee9d3c67f815ec69379cf3b55de2e90
Mr. mk <manikumar.neerugatti@outlook.com>
2022-10-20 14:47:17 +0530

    Prime

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 class System:
-   def Prime(self):
-       list = [10, 19, 3, 2, 23, 17, 20]
-       count = 0
-       for i in list:
-           for j in range(2, i):
-               if i % j == 0:
-                   count += 1
-           if count == 0:
-               print(i)
-           else:
-               list.remove(i)
+   def Prime(self):   
+        List = [4, 3, 6, 7, 9, 19, 5, 29]
+        prime = []
+        for i in List:
+            count = 0
+            for j in range(2, i):
+                if i % j == 0:
+                count += 1
+            if count == 0:
+                prime.append(i)
+        print("The Listed Prime Numbers Are: \n", prime)
 
-       print(list)
 
    def UpperAndLower(self):
        User_Input = input("Enter The UserInput: ")
@@ -34,6 +33,8 @@ s.UpperAndLower()
 
 ###---OutPut---###
 
-The Listed Prime Numbers are [19, 2, 17]
+The Listed Prime Numbers Are: 
+ [3, 7, 19, 5, 29]
+
 Enter The UserInput: MANIkumar NeeRugaTTi
 The Number of Upper case Letters are 8 They are ['M', 'A', 'N', 'I', 'N', 'R', 'T', 'T'], And Lower case Letters are 12 They Are ['k', 'u', 'm', 'a', 'r', ' ', 'e', 'e', 'u', 'g', 'a', 'i']
